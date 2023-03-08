04310fb0bd63e08907735d0bf9acb0444a8f2a08
Juan M De La Torre S <72047760+JM-delatorre@users.noreply.github.com>
2023-03-08 13:03:40 -0500

    class token

--- lexic/main.py
+++ lexic/main.py
@@ -1,1 +1,14 @@
-print("Hola mundo")
+class Token():
+    def __init__(self,id:str,row:int,col:int,lex:str = None) -> None:
+        self.id = id
+        self.row = row
+        self.col = col
+        self.lex = lex
+
+    def __repr__(self) -> str:
+        actualLex = (', ' + self.lex) if self.lex != None else ''
+        toPrint = f'<{self.id}{actualLex}, {self.row}, {self.col}>' 
+        return toPrint
+
+perro = Token('Mi ID',4,5, "Hola")
+print(perro)
